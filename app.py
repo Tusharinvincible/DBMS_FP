a5bc2351919bcf93277947a4c7cbe83f7f34dca2
VighneshPath <pathrikarvighnesh@gmail.com>
2021-12-08 17:57:05 +0530

    Fixed User Review Bug

--- app.py
+++ app.py
@@ -114,11 +114,9 @@ def review(movie_id):
     user_review =  request.form.get("user_review").strip()
     user_rating = int(request.form.get("rating"))
     # users =  db.execute("SELECT * FROM users").fetchall()
-
     movie_data = db.execute("SELECT rating_count, average_score FROM movies WHERE movieid = :movieid",{"movieid":movie_id}).fetchone()
     rating_count = movie_data[0]
     average_score = movie_data[1]
-
     if(db.execute("SELECT * FROM reviews WHERE id = :userid AND movieid = :movieid", {"userid":session["user"].id, "movieid":movie_id}).fetchone() is None):
         db.execute("INSERT INTO reviews (id, username, movieid, review, rating) VALUES (:id, :username, :movieid, :review, :rating)", {"id":session["user"].id, "username":session["user"].username, "movieid":movie_id, "review":user_review, "rating": user_rating})
         db.commit()
@@ -130,7 +128,6 @@ def review(movie_id):
     total_rating = int(average_score * rating_count)
     rating_count+=1
     average_score = (total_rating + user_rating)/rating_count
-
     db.execute("UPDATE movies SET rating_count = :rating_count, average_score = :average_score WHERE movieid = :movieid", {"rating_count":rating_count, "average_score":average_score, "movieid":movie_id})
     movie = db.execute("SELECT * FROM movies WHERE movieid = :movieid", {"movieid":movie_id}).fetchone()
     db.commit()
@@ -144,6 +141,12 @@ def edit_post():
 @app.route("/review_edit", methods = ["POST"])
 def review_edit():
     user_review =  request.form.get("user_review").strip()
+    #user_rating = int(request.form.get("rating"))
+    # users =  db.execute("SELECT * FROM users").fetchall()
+    #movie_data = db.execute("SELECT rating_count, average_score FROM movies WHERE movieid = :movieid",{"movieid":movie_id}).fetchone()
+    #rating_count = movie_data[0]
+    #average_score = movie_data[1]
+    print(user_review, request.args.get('review_id'))
     db.execute("UPDATE reviews SET review = :new_review WHERE review_id = :rev_id", {"new_review" : user_review,"rev_id":int(request.args.get('review_id'))})
     db.commit()
 
@@ -168,5 +171,4 @@ def get_movie_api(movie_id):
     movie_dict["movieid"] = movie_details.movieid
     movie_dict["review_count"] = movie_details.rating_count
     movie_dict["average_score"] = movie_details.average_score
-    
     return jsonify(movie_dict)
